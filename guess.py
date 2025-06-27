17fc81876064765077e8cf829376251325939957
Ejmin <156772450+ejoni7@users.noreply.github.com>
2025-06-27 10:26:45 +0330

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,10 @@
+word  = input("enter a word: ")
+result = list("_"  * len(word))
+print(result)
+result2 = []
+for i in word:
+    result2.append('_')
+print(result2)    
+
+result3 = ["_" for i in word]
+print(result3) 
