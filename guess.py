836ab48ca5d96192040fc1302b347d878f83f114
gsrtntuiyoyhi9pedtoK{"fukdtuerzxtg789 <aliddthty8ozt7irtz@gmail.com>
2025-07-03 09:20:39 +0330

    update2

--- guess.py
+++ guess.py
@@ -1,13 +1,12 @@
 word  = input("enter a word: ")
-result = list("_"  * len(word))
+result = list("_"  * (len(word) + 5))
 print(result)
 result2 = []
 for i in word:
     result2.append('_')
 print(result2)    
 
-result3 = ["_" for i in word]
-print(result3) 
 
 
-print("??????????")
+
+
